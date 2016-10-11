39e14d6fdc0ae285b8af4faee3508146af097a4b
Stephen Gaffney <stephen.gaffney@yale.edu>
2016-10-11 15:35:20 +0000

    Set up baf generation scripts. Add parameters.

--- baf_from_vcf.py
+++ baf_from_vcf.py
@@ -1,33 +1,40 @@
 import os
-import sys
 import subprocess
 
 import feather
-import pandas as pd
+# import pandas as pd
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-def baf_from_vcf(patient_id, vcf_path, baf_path):
+
+def baf_from_vcf(patient_id, vcf_path, baf_path, feather_path=None, col_tumor=11, col_normal=10,
+                 format_dp_index=5, mq_cutoff=30,
+                 chroms='1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT'):
     """
     Args:
         patient_id (str): used for saving saasCNV-style snp data to feather.
-    ."""
-    initial_dir = os.getcwd()
-    dest_dir = os.path.dirname(vcf_path)
-    os.chdir(dest_dir)
+        vcf_path (str): full or relative vcf path
+        col_{tumor,normal} (int): index of {tumor,normal} column in vcf. 1-based.
+        format_dp_index (int): index of DP in vcf FORMAT column. 1-based.
+        MQ_cutoff (int): cutoff for MQ, in INFO column.
 
-    table_basename = '{}_baf'.format(patient_id)
-    feather_path = os.path.join(dest_dir, table_basename + '.feather')
+    Intermediate files:
+        <baf_path>.feather: created by vcf2table.R
+    """
+    if feather_path is None:
+        feather_path = baf_path + '.feather'
 
     # RUN Rscript
     print("Running R script.")
     rscript_path = os.path.join(script_dir, 'vcf2table.R')
     # R: Parse vcf, write data to feather
-    subprocess.check_call(['Rscript', rscript_path, vcf_path, table_basename])
+    subprocess.check_call(['Rscript', rscript_path, vcf_path, feather_path,
+                           col_tumor, col_normal, format_dp_index, chroms, mq_cutoff])
+    # Import results
+    df = feather.read_dataframe(feather_path)
 
     # Finalize dataframe
     print("Finalizing dataframe.")
-    df = feather.read_dataframe(feather_path)
     df.rename(columns={'CHROM': 'chrom', 'POS': 'SNP_loc'}, inplace=True)
     df['control_doc'] = df['Normal.REF.DP'] + df['Normal.ALT.DP']
     df['tumor_doc'] = df['Tumor.REF.DP'] + df['Tumor.ALT.DP']
@@ -42,8 +49,10 @@ def baf_from_vcf(patient_id, vcf_path, baf_path):
     df.to_csv(baf_path, sep='\t', index=False)
     print("Saved BAF data to file: {}".format(baf_path))
 
-if __name__ == '__main__':
-    patient_id = sys.argv[1]
-    vcf_path = sys.argv[2]
-    baf_path = sys.argv[3]
-    baf_from_vcf(patient_id, vcf_path, baf_path)
+
+# if __name__ == '__main__':
+#     import sys
+#     patient_id = sys.argv[1]
+#     vcf_path = sys.argv[2]
+#     baf_path = sys.argv[3]
+#     baf_from_vcf(patient_id, vcf_path, baf_path)
