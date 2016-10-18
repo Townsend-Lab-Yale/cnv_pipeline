56fecad769b62fcb6937f0d12679269868d097e4
Stephen Gaffney <stephen.gaffney@yale.edu>
2016-10-18 09:08:09 +0100

    Trim vcf using GATK's SelectVariants.

---
+++ cnv_pipeline/trim_vcf.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import shlex
+
+from cnv_pipeline.config import load_config
+
+
+def trim_vcf(vcf_in=None, tumor_id=None, normal_id=None,
+             min_ratio=0.4, max_ratio=0.6, min_depth_n=10, min_depth_t=20,
+             min_gq_n=90, vcf_out=None, sample_dir=None):
+    """Create new filtered vcf file for tumor and normal sample."""
+    if normal_id is None:
+        normal_id = tumor_id + 'N'
+    if vcf_out is None:
+        vcf_out = os.path.join(sample_dir, 'snps_trimmed.vcf')
+    config = load_config()
+    gatk = config.get('gatk', 'cmd', fallback='gatk')
+    fasta = config.get('paths', 'FASTA')
+    cmd = (
+        """{gatk} -T SelectVariants -R {fasta} -V {vcf} -sn {normal} -sn {tumor} """
+        """-selectType SNP --restrictAllelesTo BIALLELIC -select 'vc.getGenotype("{normal}").isHet() """
+        """&& vc.getGenotype("{normal}").getGQ() > {gq} """
+        """&& 1.0 * vc.getGenotype("{normal}").getAD().1 /  vc.getGenotype("{normal}").getDP() > {min_ratio} """
+        """&&  1.0 * vc.getGenotype("{normal}").getAD().1 /  vc.getGenotype("{normal}").getDP() < {max_ratio} """
+        """&& vc.getGenotype("{normal}").getDP() > {min_depth_n} """
+        """&& vc.getGenotype("{tumor}").getDP() > {min_depth_t}'""")
+    cmd = cmd.format(gatk=gatk, vcf=vcf_in, fasta=fasta, normal=normal_id, tumor=tumor_id,
+                     gq=min_gq_n, min_ratio=min_ratio, max_ratio=max_ratio,
+                     min_depth_n=min_depth_n, min_depth_t=min_depth_t)
+    print("VCF trim command: {}").format(cmd)
+    with open(vcf_out, 'w') as new_vcf:
+        with open(subprocess.DEVNULL, 'r') as stdin:
+            proc = subprocess.Popen(shlex.split(cmd), stdin=stdin, stdout=new_vcf)
+    proc.communicate()
+    print("Created vcf: {}").format(vcf_out)
