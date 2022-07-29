0ff0bd59adb2f17ed5e2e809cbe7795fb512f32d
Stephen Gaffney <stephen.gaffney@yale.edu>
2022-07-29 15:10:19 +0100

    Specify bed, fasta in CLI, tidy options.

--- cnv_pipeline/pipeline.py
+++ cnv_pipeline/pipeline.py
@@ -13,9 +13,11 @@ from .trim_vcf import trim_vcf
 
 def run_cnv(vcf_path, sample_dir=None, adtex_dir=None, tumor_bam=None, normal_bam=None,
             baf_path=None, parquet_path=None, tumor_cov_path=None, normal_cov_path=None,
-            tumor_id=None, normal_id=None, saas_only=False, adtex_stdout='-',
+            tumor_id=None, normal_id=None,
+            ref_fasta=None,
+            saas_only=False, adtex_stdout='-', bed_targets=None,
             mq_cutoff=30, chroms=None, vcf_out=None,
-            target_path=None, ploidy=None, min_read_depth=10, sample_id='Tumor',
+            ploidy=None, min_read_depth=10,
             ratio_min=0.4, ratio_max=0.6, min_tumor=20, min_normal=10, min_gq=90):
     """Run pipeline.
 
@@ -42,10 +44,9 @@ def run_cnv(vcf_path, sample_dir=None, adtex_dir=None, tumor_bam=None, normal_ba
     genome_path = os.path.join(sample_dir, "genome.txt")
     if not os.path.exists(sample_dir):
         os.mkdir(sample_dir)
-    if normal_id is None:
-        normal_id = tumor_id + 'N'
 
     trim_vcf(vcf_in=vcf_path, tumor_id=tumor_id, normal_id=normal_id,
+             ref_fasta=ref_fasta,
              ratio_min=ratio_min, ratio_max=ratio_max, min_depth_n=min_normal,
              min_depth_t=min_tumor, min_gq_n=min_gq, vcf_out=vcf_out)
 
@@ -53,7 +54,7 @@ def run_cnv(vcf_path, sample_dir=None, adtex_dir=None, tumor_bam=None, normal_ba
                  tumor_id=tumor_id, normal_id=normal_id,
                  mq_cutoff=mq_cutoff, chroms_str=chroms)
 
-    run_saasCNV(sample_id=sample_id, sample_dir=sample_dir,
+    run_saasCNV(sample_id=tumor_id, sample_dir=sample_dir,
                 baf_path=parquet_path, stdout_path='-')
 
     if not saas_only:
@@ -61,13 +62,13 @@ def run_cnv(vcf_path, sample_dir=None, adtex_dir=None, tumor_bam=None, normal_ba
 
         build_coverage_files(tumor_bam=tumor_bam, normal_bam=normal_bam, genome_path=genome_path,
                              tumor_cov_path=tumor_cov_path, normal_cov_path=normal_cov_path,
-                             target_bed_path=target_path)
+                             target_bed_path=bed_targets)
 
         run_adtex(normal_cov_path=normal_cov_path,
                   tumor_cov_path=tumor_cov_path,
                   adtex_dir=adtex_dir,
                   baf_path=baf_path,
-                  target_path=target_path,
+                  target_path=bed_targets,
                   ploidy=ploidy, min_read_depth=min_read_depth,
                   stdout_path=adtex_stdout)
 
@@ -78,9 +79,7 @@ def run_saasCNV(sample_id=None, sample_dir=None, baf_path=None, stdout_path='-')
     """Example call from bash:
     Rscript run_saas.R {s_id} {sample_dir} {baf_path} 50 30 FALSE 0.05 0.05
     """
-    script_path = os.path.join(this_dir, 'run_saas.R')
-    config = load_config()
-    rscript_path = config.get('paths', 'RSCRIPT', fallback='Rscript')
+    script_path = os.path.join(PKG_DIR_PATH, 'run_saas.R')
     sample_path = os.path.realpath(sample_dir)
     baf_path = os.path.realpath(baf_path)
     cmd = (f"Rscript {script_path} {sample_id} {sample_path} {baf_path}"
@@ -160,20 +159,22 @@ def main():
     parser = argparse.ArgumentParser("CNV PIPELINE")
     parser.add_argument('-v', '--vcf', help='VCF file for sample pair', required=True)
     parser.add_argument('-s', '--sample_dir', help='Sample-specific output dir', required=True)
-    parser.add_argument('-t', '--tumor', help='Tumor BAM', required=False)
-    parser.add_argument('-n', '--normal', help='Normal BAM', required=False)
+    parser.add_argument('-t', '--tumor', help='Tumor BAM', required=True)
+    parser.add_argument('-n', '--normal', help='Normal BAM', required=True)
     parser.add_argument('-tid', '--tumor_id', help='Tumor name, for vcf extraction', required=True)
     parser.add_argument('-nid', '--normal_id', help='Normal name, for vcf extraction', required=True)
-
+    parser.add_argument('-R', '--ref-fasta', help='Reference genome fasta path', required=True)
     parser.add_argument('--saas_only', help='Only run saasCNV, not ADTEx.', action='store_true', default=False)
-
+    # VCF filtering arguments
     parser.add_argument('-rmin', '--ratio_min', help='Min alt ratio in normal sample [0.4]', type=float, default=0.4)
     parser.add_argument('-rmax', '--ratio_max', help='Max alt ratio in normal sample [0.6]', type=float, default=0.6)
     parser.add_argument('-tmin', '--min_tumor', help='Min reads from tumor sample [20]', type=int, default=20)
     parser.add_argument('-nmin', '--min_normal', help='Min reads from normal sample [10]', type=int, default=10)
     parser.add_argument('-gq', '--min_gq', help='Genotype quality cutoff for normal sample [90]', type=int, default=90)
-    parser.add_argument('-a', '--adtex_dir', help='ADTEx output dir', default=None)
-    parser.add_argument("--ploidy", help="Most common ploidy in the tumour sample", type=int, default=None)
+    # ADTEx-specific
+    parser.add_argument('-a', '--adtex_dir', help='ADTEx: output dir', default=None)
+    parser.add_argument('-b', '--bed', help='ADTEx: BED file for targeted regions', default=None)
+    parser.add_argument("--ploidy", help="ADTEx: most common ploidy in the tumour sample", type=int, default=None)
     parser.add_argument("--minReadDepth", help="The ADTEx threshold for minimum read depth for each exon [10]",
                         type=int, default=10)
     parser.add_argument('-ao', '--adtex_stdout', help='ADTEx stdout path if overriding STDOUT', default='-')
@@ -185,6 +186,8 @@ def main():
     run_cnv(vcf_path=args.vcf, sample_dir=args.sample_dir, adtex_dir=args.adtex_dir,
             tumor_bam=args.tumor, normal_bam=args.normal,
             tumor_id=args.tumor_id, normal_id=args.normal_id,
+            ref_fasta=args.ref_fasta,
+            bed_targets=args.bed,
             saas_only=args.saas_only,
             ploidy=args.ploidy, min_read_depth=args.minReadDepth,
             adtex_stdout=args.adtex_stdout, **vcf_dict)
